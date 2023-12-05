c0c0b03ac8156f4d03f9dd0c4a57fa22762048ec
Judy <143587407+Judy770@users.noreply.github.com>
2023-12-05 12:39:26 +0100

    Create code_1.py

---
+++ code_1.py
@@ -0,0 +1,27 @@
+import random
+
+
+def generate_random_data():
+
+        print(f"Random Number: {item}")
+    data = generate_random_data()
+    for item in data:
+
+if __name__ == "__main__":
+    main()
+def main():
+    return data
+    data = [random.randint(1, 100) for _ in range(10)]if __name__ == "__main__":
+    main()
+
+def main():
+
+    for item in data:
+def generate_random_data():
+        print(f"Random Number: {item}")
+    return data
+
+import random
+    data = generate_random_data()
+
+    data = [random.randint(1, 100) for _ in range(10)]
