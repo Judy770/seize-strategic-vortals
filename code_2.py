0119414a1aa84f47510c69507c3bef579ad46f3b
Judy <143587407+Judy770@users.noreply.github.com>
2023-12-13 22:17:59 +0100

    Create code_2.py

---
+++ code_2.py
@@ -0,0 +1,29 @@
+        }
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Random Number: " + item);
+            data[i] = (int) (Math.random() * 100) + 1;
+    }
+}
+        }
+
+    public static void main(String[] args) {
+        int[] data = new int[10];
+        for (int item : data) {
+
+public class RandomDataGenerator {}
+
+}
+
+    });
+    const data = [];
+main();
+    for (let i = 0; i < 10; i++) {
+    return data;
+    data.forEach(item => {
+        console.log("Random Number: " + item);
+        data.push(Math.floor(Math.random() * 100) + 1);
+
+function generateRandomData() {
+function main() {
+    }
+    const data = generateRandomData();
